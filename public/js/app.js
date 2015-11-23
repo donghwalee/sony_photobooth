8c1688cd8599b2718fc98cde9a11047b466b3324
Donghwa Lee <donghwalee328@gmail.com>
2015-11-23 15:47:26 -0500

    logging of orientation, orientation of the image,  and margins of divs surrounding the image changes according to orientation of the camera

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+var getStatus      = "{\r\"method\": \"getEvent\",\r\"params\": [true],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
 var takePicture    = "{\r\"method\": \"actTakePicture\",\r\"params\": [],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
 var zoomIn         = "{\r\"method\": \"actZoom\",\r\"params\": [\"in\",\"1shot\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
 var zoomOut        = "{\r\"method\": \"actZoom\",\r\"params\": [\"out\",\"1shot\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
@@ -14,6 +15,7 @@ $(function(){
   //TAKE PICTURE
   $('#take-picture').click(function(event) {
     event.preventDefault();
+    getOrientation();
     var promise = $.ajax({
       url: 'http://10.0.0.1:10000/sony/camera',
       method: 'POST',
@@ -22,15 +24,32 @@ $(function(){
       data: takePicture
     });
     promise.done(function(data) {
+      $('#picture-area').empty();
       imageURL = data.result[0][0];
       data.result.forEach(function(pic){
-        $('body').append(
-          '<img src="'+pic+'" class="result"></img><br /><hr /><br /><button id="print-image">PRINT</button>'
+        $('#picture-area').append(
+          '<img src="'+pic+'" id="rotate"></img><div id="result-controls"><hr /><button id="print-image">PRINT</button><button id="fb-share">SHARE ON FACEBOOK</button></div>'
         );
+        $('#rotate').css({
+          WebkitTransform: 'rotate(' + orientation + 'deg)'
+        });
+        $('#rotate').css({
+          '-moz-transform': 'rotate(' + orientation + 'deg)'
+        });
+        if (orientation == 90 || orientation == 270) {
+          $('#result-controls').css({'margin-top': $('#rotate').width()/7});
+          $('#top-controls').css({'margin-bottom': $('#rotate').width()/7});
+        } else {
+          $('#rotate-controls').css({'margin-top': 0});
+          $('#top-controls').css({'margin-bottom': 0});
+        }
         $('#print-image').click(function() {
           var newWindow = window.open(imageURL);
           newWindow.window.print();
           // newWindow.window.close();
+        });
+        $('#fb-share').click(function() {
+          console.log(imageURL);
         });
       });
     });
@@ -75,9 +94,58 @@ $(function(){
     });
   });
 
+  //GET STATUS
+  // $('#get-status').click(function(event) {
+  //   event.preventDefault();
+  //   var promise = $.ajax({
+  //     url: 'http://10.0.0.1:10000/sony/camera',
+  //     method: 'POST',
+  //     dataType: 'json',
+  //     processData: false,
+  //     data: getStatus
+  //   });
+  //   promise.done(function(data) {
+  //     if (data.result == undefined || data.result[4] == null) {
+  //       console.log("no change or error");
+  //     } else if (data.result[4] === 0 || 90 || 180 || 270 ) {
+  //       orientation = data.result[4].liveviewOrientation;
+  //       console.log(orientation);
+  //     }
+  //   });
+  //   promise.error(function(data) {
+  //     console.log("error");
+  //   });
+  // });
+
+  getOrientation = function() {
+    var promise = $.ajax({
+      url: 'http://10.0.0.1:10000/sony/camera',
+      method: 'POST',
+      dataType: 'json',
+      processData: false,
+      data: getStatus
+    });
+    promise.done(function(data) {
+      if (data.result == undefined || data.result[4] == null) {
+        console.log("no change or error");
+      } else if (data.result[4] === 0 || 90 || 180 || 270 ) {
+        orientation = data.result[4].liveviewOrientation;
+        console.log(orientation);
+      }
+    });
+    promise.error(function(data) {
+      console.log("error");
+    });
+  }
+
 });
 
 
 // PRINT VAR & FUNCTION
 
 var imageURL = "";
+
+
+// ORIENTATION
+
+var orientation = 0;
