0eab5271ee84f1adc7395ebc9002274d60775aec
Donghwa Lee <donghwalee328@gmail.com>
2015-11-22 22:48:23 -0500

    take picture and show on body

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,68 @@
+var takePicture   = "{\r\"method\": \"actTakePicture\",\r\"params\": [],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
+var zoomIn        = "{\r\"method\": \"actZoom\",\r\"params\": [\"in\",\"1shot\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
+var zoomOut       = "{\r\"method\": \"actZoom\",\r\"params\": [\"out\",\"1shot\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
+var zoomInFull    = "{\r\"method\": \"actZoom\",\r\"params\": [\"in\",\"start\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
+var zoomOutFull   = "{\r\"method\": \"actZoom\",\r\"params\": [\"out\",\"start\"],\r\"id\": 1,\r\"version\": \"1.0\"\r}";
 
 
-
+// AJAX FUNCTIONS
 
 $(function(){
-  $.ajax({
-    url: 'http://10.0.0.1:10000/sony/camera',
-    method: 'POST',
-    dataType: 'json',
-    processData: false,
-    data: "{\r\"method\": \"actTakePicture\",\r\"params\": [],\r\"id\": 1,\r\"version\": \"1.0\"\r}"
-    ,
-    success: function (data, status, jqXHR) {
-      console.log("success");
-      console.log(data);
-      console.log(status);
-      console.log(jqXHR);
-    },
-    error: function (jqXHR, status, error) {
+
+  //TAKE PICTURE
+  $('#take-picture').click(function(event) {
+    event.preventDefault();
+    var promise = $.ajax({
+      url: 'http://10.0.0.1:10000/sony/camera',
+      method: 'POST',
+      dataType: 'json',
+      processData: false,
+      data: takePicture
+    });
+    promise.done(function(data) {
+      data.result.forEach(function(item){
+        $('body').append('<img src="'+item+'" class="result"></img>');
+      });
+    });
+    promise.error(function(data) {
       console.log("error");
-      console.log(jqXHR);
-      console.log(status);
-      console.log(error);
-    }
+    });
   });
+
+  //ZOOM IN SMALL
+  $('#zoom-in').click(function(event) {
+    event.preventDefault();
+    var promise = $.ajax({
+      url: 'http://10.0.0.1:10000/sony/camera',
+      method: 'POST',
+      dataType: 'json',
+      processData: false,
+      data: zoomIn
+    });
+    promise.done(function(data) {
+      console.log(data);
+    });
+    promise.error(function(data) {
+      console.log("error");
+    });
+  });
+
+  //ZOOM OUT SMALL
+  $('#zoom-out').click(function(event) {
+    event.preventDefault();
+    var promise = $.ajax({
+      url: 'http://10.0.0.1:10000/sony/camera',
+      method: 'POST',
+      dataType: 'json',
+      processData: false,
+      data: zoomOut
+    });
+    promise.done(function(data) {
+      console.log(data);
+    });
+    promise.error(function(data) {
+      console.log("error");
+    });
+  });
+
 });
